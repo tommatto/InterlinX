4add72dc138baaf01a3dde29103735188151e879
tommatto <35725621+tommatto@users.noreply.github.com>
2018-02-14 12:23:36 +0530

    Splash screen

---
+++ app/src/main/java/com/interlinks/interlinx/signup/InterlinxLogin.java
@@ -0,0 +1,22 @@
+package com.interlinks.interlinx.signup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.interlinks.interlinx.R;
+
+/**
+ * Created by Admin on 05-02-2018.
+ */
+
+public class InterlinxLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+
+
+    }
+}
